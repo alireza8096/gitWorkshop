873a44e2ef8ce5b432e89a5b631745469b0ce881
Alireza Hosseinkhani <alireza.8096@chmail.ir>
2020-04-18 23:27:46 +0430

    6

--- src/Quera.java
+++ src/Quera.java
@@ -2,7 +2,11 @@ public class Quera {
     public static void main(String[] args) {
         while (true) {
 
-            System.out.println("hello");
+        int x = 1;
+        if (x == 1) {
+            System.out.println("x = 1");
+            break;
+        }
         }
     }
 }
